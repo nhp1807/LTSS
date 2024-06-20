e053613b49a664f28baf5d98302d124ab4dd9663
Phong Nguyen <nhp1807@gmail.com>
2024-06-20 23:35:14 +0700

    fix bug

--- random_mpi.c
+++ random_mpi.c
@@ -179,7 +179,7 @@ int main(int argc, char *argv[])
   printf("v: %d\n", v);
 
   printf("  %4d  %4d                %12d --\n", k, id, v);
-  
+
   /*
     Sử dụng LCRG để tính toán các giá trị với chỉ số ID, ID + P, ID + 2P, ...
   */
@@ -188,7 +188,6 @@ int main(int argc, char *argv[])
     u = v;
     v = lcrg_evaluate(an, bn, c, u);
     // printf("  %4d  %4d  %12d  %12d ---\n", k, id, u, v);
-
   }
 
   // /*
@@ -251,52 +250,52 @@ int main(int argc, char *argv[])
   // }
   // return 0;
   int final_value = v;
-    int *all_final_values = NULL;
-    if (id == 0)
+  int *all_final_values = NULL;
+  if (id == 0)
+  {
+    all_final_values = (int *)malloc(p * sizeof(int));
+  }
+
+  com_start_time = MPI_Wtime();
+  MPI_Gather(&final_value, 1, MPI_INT, all_final_values, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  com_end_time = MPI_Wtime();
+  total_com_time = total_com_time + com_end_time - com_start_time;
+
+  if (id == 0)
+  {
+    for (int i = 0; i < p; i++)
     {
-        all_final_values = (int *)malloc(p * sizeof(int));
+      printf("Processor %d final value: %d\n", i, all_final_values[i]);
     }
+    free(all_final_values);
+  }
 
-    com_start_time = MPI_Wtime();
-    MPI_Gather(&final_value, 1, MPI_INT, all_final_values, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    com_end_time = MPI_Wtime();
-    total_com_time = total_com_time + com_end_time - com_start_time;
+  end_time = MPI_Wtime();
+  total_time = end_time - start_time;
 
-    if (id == 0)
-    {
-        for (int i = 0; i < p; i++)
-        {
-            printf("Processor %d final value: %d\n", i, all_final_values[i]);
-        }
-        free(all_final_values);
-    }
+  MPI_Reduce(&total_time, &total_time_show, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
-    end_time = MPI_Wtime();
-    total_time = end_time - start_time;
+  if (id == 0)
+  {
+    printf("Total communication time: %f\n", total_com_time);
+    printf("Total execution time (T_w_com): %f seconds.\n", total_time);
+    printf("Execution time without communication (T_wo_com): %f seconds.\n", total_time - total_com_time);
+    printf("Each processor has %d numbers\n", number_each_process);
+    printf("Total number of random numbers: %d\n", total_number);
+    printf("Total of processors: %d\n", p);
+  }
 
-    MPI_Reduce(&total_time, &total_time_show, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+  MPI_Finalize();
 
-    if (id == 0)
-    {
-        printf("Total communication time: %f\n", total_com_time);
-        printf("Total execution time (T_w_com): %f seconds.\n", total_time);
-        printf("Execution time without communication (T_wo_com): %f seconds.\n", total_time - total_com_time);
-        printf("Each processor has %d numbers\n", number_each_process);
-        printf("Total number of random numbers: %d\n", total_number);
-        printf("Total of processors: %d\n", p);
-    }
-
-    MPI_Finalize();
-
-    if (id == 0)
-    {
-        printf("\n");
-        printf("RANDOM_MPI:\n");
-        printf("  Normal end of execution.\n");
-        printf("\n");
-        timestamp();
-    }
-    return 0;
+  if (id == 0)
+  {
+    printf("\n");
+    printf("RANDOM_MPI:\n");
+    printf("  Normal end of execution.\n");
+    printf("\n");
+    timestamp();
+  }
+  return 0;
 }
 
 int congruence(int a, int b, int c, int *error)
